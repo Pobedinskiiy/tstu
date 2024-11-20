6d10feae4adfcdde70c63a5e844ef963845c4815
pobedimka <111460880+Pobedinskiiy@users.noreply.github.com>
2024-11-20 03:28:26 +0300

    Update main.py

--- MODS/laboratory work 2.6/main.py
+++ MODS/laboratory work 2.6/main.py
@@ -2,237 +2,82 @@ import numpy as np
 import warnings
 
 
-def simplex(type, A, B, C, D, M):
-    """Calculates an optimal point for the linear programming model given by A*x <= B , Optimize z= C' * x
-
-    Arguments:
-    type -- type of optimization, it can be 'max' or 'min'
-    A    -- A matrix of the model (numpy array)
-    B    -- B matrix of the model, column vector (numpy array)
-    C    -- C matrix of the model, column vector (numpy array)
-    D    -- column vector with the types of restrictions of the model (numpy array), 1 is <=, 0 is =, -1 is >=
-            for <= restrictions do nothing
-            for = restrictions add an artificial variables and a big M in the objective
-            function (min --> +M , max --> -M)
-            for >= restrictions multiply by -1
-    M    -- big M value
-    """
-
-    # m -- number of restrictions
-    # n -- number of variables
+def simplex(opt_type, A, B, C, D, M):
     (m, n) = A.shape
-
     basic_vars = []
     count = n
-
-    # matrix with new variables
     R = np.eye(m)
 
-    # values of the new variables
-    P = B
-
-    # artificial variables position indicator
-    artificial = []
-
     for i in range(m):
+        basic_vars.append(count)
         if D[i] == 1:
-            # add the slack variable to objective function
             C = np.vstack((C, [[0]]))
-
-            # regist the slack variable as basic variable
-            count = count + 1
-            basic_vars = basic_vars + [count - 1]
-
-            artificial = [artificial, 0]
-
+            count += 1
         elif D[i] == 0:
-            # add the artificial variable to objective function with the big M value
-            if type == 'min':
-                C = np.vstack((C, [[M]]))
-            else:
-                C = np.vstack((C, [[-M]]))
-
-            # regist the artificial variable as basic variable
-            count = count + 1
-            basic_vars = basic_vars + [count - 1]
-
-            artificial = [artificial, 1]
+            C = np.vstack((C, [[M if opt_type == 'min' else -M]]))
+            count += 1
         elif D[i] == -1:
-            # add the surplus and artificial variables to objective function
-            if type == 'min':
-                C = np.vstack((C, [[0], [M]]))
-            else:
-                C = np.vstack((C, [[0], [-M]]))
-
+            C = np.vstack((C, [[0], [M if opt_type == 'min' else -M]]))
             R = repeatColumnNegative(R, count + 1 - n)
-            P = insertZeroToCol(P, count + 1 - n)
+            count += 2
 
-            # regist the artificial variable as basic variable
-            count = count + 2
-            basic_vars = basic_vars + [count - 1]
-
-            artificial = [artificial, 0, 1]
-        else:
-            print("invalid case")
-
-    # current vertex
-    X = np.vstack((np.zeros((n, 1)), P))
-
-    # add new variables to matrix A
     A = np.hstack((A, R))
-
-    # simplex tableau
     st = np.vstack((np.hstack((-np.transpose(C), np.array([[0]]))), np.hstack((A, B))))
-
-    # number of columns
-    (rows, cols) = st.shape
-
-    # basic_vars = ((n + 1):n+m)'
-
-    print('\nsimplex tableau\n')
-    print(st)
-    print('\ncurrent basic variables\n')
-    print(basic_vars)
-    print('\noptimal point\n')
-    print(X)
-
-    # check if z != 0 (when there are artificial variables)
-    z_optimal = np.matmul(np.transpose(C), X)
-
-    print('\ncurrent Z\n\n', z_optimal)
+    z_optimal = C.T @ np.vstack((np.zeros((n, 1)), insertZeroToCol(B, count + 1 - n)))
+    X = np.zeros((count, 1))
 
     if z_optimal != 0:
         for i in range(m):
-            if D[i] == 0 or D[i] == -1:
-                if type == 'min':
-                    st[0, :] = st[0, :] + M * st[1 + i, :]
-                else:
-                    st[0, :] = st[0, :] - M * st[1 + i, :]
+            if D[i] in np.array([0, -1]):
+                st[0, :] += [M if opt_type == 'min' else -M] * st[i + 1, :]
 
-        print('\ncorrected simplex tableau\n')
-        print(st)
-
-    iteration = 0
     while True:
-        # for zz in range(2):
-        if type == 'min':
-            # select the more positive value
-            w = np.amax(st[0, 0:cols - 1])
-            iw = np.argmax(st[0, 0:cols - 1])
-        else:
-            # select the more negative value
-            w = np.amin(st[0, 0:cols - 1])
-            iw = np.argmin(st[0, 0:cols - 1])
+        w = np.amax(st[0, :-1]) if opt_type == 'min' else np.amin(st[0, :-1])
+        iw = np.argmax(st[0, :-1]) if opt_type == 'min' else np.argmin(st[0, :-1])
 
-        if w <= 0 and type == 'min':
-            print('\nGlobal optimum point\n')
+        if (w <= 0 and opt_type == 'min') or (w >= 0 and opt_type == 'max'):
             break
-        elif w >= 0 and type == 'max':
-            print('\nGlobal optimum point\n')
-            break
-        else:
-            iteration = iteration + 1
 
-            print('\n----------------- Iteration {} -------------------\n'.format(iteration))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            T = st[1:, -1] / st[1:, iw]
 
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                T = st[1:rows, cols - 1] / st[1: rows, iw]
+        R = np.logical_and(T != np.inf, T > 0)
+        k, ik = minWithMask(T, R)
 
-            R = np.logical_and(T != np.inf, T > 0)
-            (k, ik) = minWithMask(T, R)
+        cz = st[[0], :]
 
-            # current z row
-            cz = st[[0], :]
+        pivot = st[ik + 1, iw]
+        prow = st[ik + 1, :] / pivot
 
-            # pivot element
-            pivot = st[ik + 1, iw]
+        st -= st[:, [iw]] * prow
+        st[ik + 1, :] = prow
 
-            # pivot row divided by pivot element
-            prow = st[ik + 1, :] / pivot
+        basic_vars[ik] = iw
+        basic = st[:, -1]
 
-            st = st - st[:, [iw]] * prow
+        for k in range(np.size(basic_vars) - 1):
+            X[basic_vars[k]] = basic[k + 1]
 
-            # pivot row is a special case
-            st[ik + 1, :] = prow
+        z_optimal = cz[0, -1] - cz[[0], :count] @ X
+        st[0, -1] = z_optimal
 
-            # new basic variable
-            basic_vars[ik] = iw
-
-            print('\ncurrent basic variables\n')
-            print(basic_vars)
-
-            # new vertex
-            basic = st[:, cols - 1]
-            X = np.zeros((count, 1))
-
-            t = np.size(basic_vars)
-
-            for k in range(t):
-                X[basic_vars[k]] = basic[k + 1]
-
-            print('\ncurrent optimal point\n')
-            print(X)
-
-            # new z value
-            C = -np.transpose(cz[[0], 0:count])
-
-            z_optimal = cz[0, cols - 1] + np.matmul(np.transpose(C), X)
-            st[0, cols - 1] = z_optimal
-
-            print('\nsimplex tableau\n\n')
-            print(st)
-
-            print('\ncurrent Z\n\n')
-            print(z_optimal)
-
-    # check if some artificial variable is positive (infeasible solution)
-    tv = np.size(artificial)
-    for i in range(tv):
-        if artificial[i] == 1:
-            if X[n + i] > 0:
-                print('\ninfeasible solution\n')
-                break
-
-    return (z_optimal[0, 0], X)
+    return X, z_optimal[0, 0]
 
 
 def minWithMask(x, mask):
-    min = 0
-    imin = 0
-
-    n = np.size(x)
-
-    for i in range(n):
-        if mask[i] == 1:
-            if min == 0:
-                min = x[i]
-                imin = i
-            else:
-                if min > x[i]:
-                    min = x[i]
-                    imin = i
-
-    return (min, imin)
+    masked_x = np.where(mask == 1, x, np.inf)
+    return np.min(masked_x), np.argmin(masked_x)
 
 
-def repeatColumnNegative(Mat, h):
-    """Repeat column h multiplied by - 1"""
-    (r, c) = Mat.shape
-    Mat = np.hstack((Mat[:, 0:h - 1], -Mat[:, [h - 1]], Mat[:, h - 1:c]))
-
-    return Mat
+def repeatColumnNegative(mat, h):
+    return np.hstack((mat[:, :h - 1], -mat[:, [h - 1]], mat[:, h - 1:np.size(mat)]))
 
 
 def insertZeroToCol(col, h):
-    """insert zero to column"""
-    k = np.size(col)
-    col = np.vstack((col[0:h - 1, [0]], np.array([[0]]), col[h - 1:k, [0]]))
-
-    return col
+    return np.vstack((col[:h - 1, [0]], np.array([[0]]), col[h - 1:np.size(col), [0]]))
 
 
-# Поиск оптимального решения
 solution, optimal_value = simplex("min",
                                   np.array([[2.4, 5.2],
                                             [1, 0],
@@ -242,5 +87,5 @@ solution, optimal_value = simplex("min",
                                   np.array([[-1], [1], [1]]),
                                   100)
 
-print(f"Оптимальное решение: x1 = {solution}")
-print(f"Максимальное значение Z = {optimal_value}")
+print(f"Оптимальное решение: x1 = {solution[0, 0]:.4f}, x2 = {solution[1, 0]:.4f}")
+print(f"Минимальное значение Z = {optimal_value:.4f}")
