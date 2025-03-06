ffe08c4cc29ed49b40f6c9a36429191b819d1640
pobedimka <pobedinskydmitryaleksandrovich@yandex.ru>
2025-03-06 14:56:41 +0300

    + Added the first lab on the subject of Maistrenko

--- foitac/laboratory-2/main.py
+++ foitac/laboratory-2/main.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import Qt
 from PIL import Image
 from image_converter import Ui_ImageConverter
 import itertools
+import re
 
 
 class ImageToGrayscale(QMainWindow, Ui_ImageConverter):
@@ -17,6 +18,7 @@ class ImageToGrayscale(QMainWindow, Ui_ImageConverter):
 
         self.selectButton.clicked.connect(self.select_image)
         self.saveButton.clicked.connect(self.save_image)
+        self.unpackButton.clicked.connect(self.unpacked)
 
         self.scene = QGraphicsScene(self)
         self.graphicsView.setScene(self.scene)
@@ -72,16 +74,66 @@ class ImageToGrayscale(QMainWindow, Ui_ImageConverter):
 
     def save_image(self):
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getSaveFileName(self, "Сохранить файл", "", "Text Files (*.txt);;All Files (*)", options=options)
-
+        file_name, _ = QFileDialog.getSaveFileName(self, "Сохранить изображение", "",
+                                                   "Изображение (*.txt);;Все файлы (*)",
+                                                   options=options)
         if file_name:
             try:
-                with open(file_name, 'w', encoding='utf-8') as file:
+                with open(file_name, 'w') as file:
                     file.write(self.image_txt)
                 QMessageBox.information(self, "Сохранение", f"Файл '{file_name}' успешно сохранен.")
             except Exception as e:
                 QMessageBox.critical(self, "Ошибка", f"Не удалось сохранить файл: {str(e)}")
 
+    def unpacked(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                  "Изображения (*.txt);;Все файлы (*)",
+                                                  options=options)
+
+        text = []
+
+        if fileName:
+            try:
+                with open(fileName, 'r') as file:
+                    text = file.readlines()
+                QMessageBox.information(self, "Открыт", "Файл успешно открыт")
+            except Exception as e:
+                QMessageBox.critical(self, "Ошибка", f"Не удалось открыть файл: {e}")
+                return
+
+        width, height = text[1].split()
+        image_txt = text[0][:-1]
+
+        decoded_image = np.zeros((int(height), int(width)), dtype=np.uint8)
+
+        current_row = 0
+        current_col = 0
+
+        fragments = re.findall(r'(\d+)([bw])', image_txt)  # Используем регулярное выражение для парсинга
+
+        if not fragments:
+            print("Ошибка: Неверный формат image_txt.")
+            return None
+
+        try:
+            for count, color in fragments:
+                count = int(count)
+                color_value = 0 if color == 'b' else 255
+
+                for _ in range(count):
+                    decoded_image[current_row, current_col] = color_value
+                    current_col += 1
+
+                    if current_col == width:
+                        current_col = 0
+                        current_row += 1
+
+                    if current_row == height:
+                        break
+
+        except Exception as e:
+            print(f"Ошибка при декодировании: {e}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
